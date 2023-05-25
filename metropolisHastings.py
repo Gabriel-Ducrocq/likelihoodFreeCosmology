9d7bf033bda289c707b00d1825f9abecbc7486da
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2023-05-25 11:52:21 +0200

    again

--- metropolisHastings.py
+++ metropolisHastings.py
@@ -57,7 +57,7 @@ print("observed CLS shape:", observed_cls.shape)
 COSMO_PARAMS_NAMES = ["n_s", "omega_b", "omega_cdm", "100*theta_s", "ln10^{10}A_s", "tau_reio"] # Parameters names
 COSMO_PARAMS_MEAN_PRIOR = np.array([0.9665, 0.02242, 0.11933, 1.04101, 3.047, 0.0561]) # Prior mean
 COSMO_PARAMS_SIGMA_PRIOR = np.array([0.0038, 0.00014, 0.00091, 0.00029, 0.014, 0.0071]) # Prior std
-proposal_std = COSMO_PARAMS_SIGMA_PRIOR*0.07
+proposal_std = COSMO_PARAMS_SIGMA_PRIOR*0.05
 prior_std = COSMO_PARAMS_SIGMA_PRIOR
 prior_mean = COSMO_PARAMS_MEAN_PRIOR
 
